835e2fa8cac3a793e5b7f92d3dd08154f49c9079
Ayushi <ayushisinha440@gmail.com>
2023-12-25 22:36:49 +0530

    first commit of this project

---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://ayushisinha440:l7YymDOogjeHh6fS@yelp-cluster.9vo69ny.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+//mongoose.connect("mongodb://127.0.0.1/test");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city},${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
+  const c = new Campground({ title: "purple field" });
+  await c.save();
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
