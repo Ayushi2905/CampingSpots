b854a47524614c5bfa39fb2dbad0fb0d6647ec05
Ayushi <ayushisinha440@gmail.com>
2024-01-02 22:51:00 +0530

    feat:added maps to campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -20,10 +20,11 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      //your user id
       author: "658fe70c0756009dfde262d8",
       location: `${cities[random1000].city},${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
@@ -31,6 +32,13 @@ const seedDB = async () => {
       description:
         "camping ventures are the best kinda experiences for nature lovers!",
       price,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       images: [
         {
           url: "https://res.cloudinary.com/dgmccrols/image/upload/v1704018175/CampSpots/ty4vrn0wful3wo2bnjtx.jpg",
